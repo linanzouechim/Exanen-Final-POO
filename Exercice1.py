578ea37aec8e810da7f9d58672e4bd6f58022ed9
Lina NZOUECHIM <linanzouechim@gmail.com>
2026-01-15 14:36:08 -0500

    modif Exercice 1

--- Exercice1.py
+++ Exercice1.py
@@ -1,9 +1,23 @@
 class Triangle() :
+    def __init__(self,n):
+        self.n = n
+    def construire_Triangle(self):
+        return ('*')
+
+
     pass
 
 
 class Affichage():
-    pass
+    def __init__(self,Tri ):
+        self.Tri = Tri
+
+    def Affichage_Triangle(self):
+        print(Tri)
+
+
+
+
 yes=1
 while yes ==1:
     try:
@@ -11,4 +25,10 @@ while yes ==1:
         yes=0
     except ValueError:
         print("Veuillez saisir un nombre valide")
+
+for i in range(1,n+1):
+    t = Triangle(i)
+    aff = t.construire_Triangle()
+    a = Affichage(aff)
+    a.Affichage_Triangle()
 
